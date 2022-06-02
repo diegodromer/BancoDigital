55c78a731ca5b2aba9ec95a808a982bbd2aef58f
Diego Lima de Oliveira <diego.dromer@hotmail.com>
2022-06-01 22:47:23 -0300

    tela de cadastro de usuário criado.

---
+++ app/src/main/java/com/diegolima/bancodigital/autenticacao/CadastroActivity.java
@@ -0,0 +1,126 @@
+package com.diegolima.bancodigital.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.diegolima.bancodigital.MainActivity;
+import com.diegolima.bancodigital.R;
+import com.diegolima.bancodigital.autenticacao.model.Usuario;
+import com.diegolima.bancodigital.helper.FirebaseHelper;
+import com.google.firebase.database.DatabaseReference;
+
+public class CadastroActivity extends AppCompatActivity {
+
+	private EditText edtNome;
+	private EditText edtEmail;
+	private EditText edtTelefone;
+	private EditText edtSenha;
+	private EditText edtConfirmaSenha;
+	private ProgressBar progressBar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro);
+
+		iniciaComponentes();
+	}
+
+	public void validaDados(View view) {
+		String nome = edtNome.getText().toString();
+		String email = edtEmail.getText().toString();
+		String telefone = edtTelefone.getText().toString();
+		String senha = edtSenha.getText().toString();
+		String confirmaSenha = edtConfirmaSenha.getText().toString();
+
+		if (!nome.isEmpty()) {
+			if (!email.isEmpty()) {
+				if (!telefone.isEmpty()) {
+					if (!senha.isEmpty()) {
+						if (!confirmaSenha.isEmpty()) {
+							if (senha.equals(confirmaSenha)) {
+
+								progressBar.setVisibility(View.VISIBLE);
+
+								Usuario usuario = new Usuario();
+								usuario.setNome(nome);
+								usuario.setEmail(email);
+								usuario.setTelefone(telefone);
+								usuario.setSenha(senha);
+								usuario.setSaldo(0);
+
+								cadastrarUsuario(usuario);
+
+							} else {
+								edtSenha.setError("Senhas diferentes.");
+								edtConfirmaSenha.setError("Senhas diferentes.");
+							}
+						} else {
+							edtConfirmaSenha.requestFocus();
+							edtConfirmaSenha.setError("Confirme sua senha.");
+						}
+					} else {
+						edtSenha.requestFocus();
+						edtSenha.setError("Informe sua senha.");
+					}
+				} else {
+					edtTelefone.requestFocus();
+					edtTelefone.setError("Informe seu telefone.");
+				}
+			} else {
+				edtEmail.requestFocus();
+				edtEmail.setError("Informe seu email.");
+			}
+		} else {
+			edtNome.requestFocus();
+			edtNome.setError("Informe seu nome.");
+		}
+	}
+
+	private void cadastrarUsuario(Usuario usuario) {
+		FirebaseHelper.getAuth().createUserWithEmailAndPassword(
+				usuario.getEmail(), usuario.getSenha()
+		).addOnCompleteListener(task -> {
+			if (task.isSuccessful()) {
+				String id = task.getResult().getUser().getUid();
+				usuario.setId(id);
+
+				salvarDadosUsuario(usuario);
+			} else {
+				progressBar.setVisibility(View.GONE);
+				Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void salvarDadosUsuario(Usuario usuario) {
+
+		DatabaseReference usuarioRef = FirebaseHelper.getDatabaseReference()
+				.child("usuarios")
+				.child(usuario.getId());
+		usuarioRef.setValue(usuario).addOnCompleteListener(task -> {
+			if (task.isSuccessful()){
+				startActivity(new Intent(this, MainActivity.class));
+				finish();
+			}else{
+				progressBar.setVisibility(View.GONE);
+				Toast.makeText(this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void iniciaComponentes() {
+		edtNome = findViewById(R.id.edtNome);
+		edtEmail = findViewById(R.id.edtEmail);
+		edtTelefone = findViewById(R.id.edtTelefone);
+		edtSenha = findViewById(R.id.edtSenha);
+		edtConfirmaSenha = findViewById(R.id.edtConfirmaSenha);
+		progressBar = findViewById(R.id.progressBar);
+	}
+}
---
+++ app/src/main/java/com/diegolima/bancodigital/autenticacao/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.diegolima.bancodigital.autenticacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.diegolima.bancodigital.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
